26dcfe0454bec2653312a97411db08b331a62133
ouss225 <oussamazerox9@gmail.com>
2022-05-06 11:49:32 +0000

    BONJOUR

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,7 +3,7 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		final int index;
+		//final int index;
 		for(int i = 0; i<5; i++) {
 			System.out.println("Hello bugs !");
 		}
