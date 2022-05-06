7c6b89b17ef38e0fb0b014fd1c7fb9ead74a1f3f
ouss225 <oussamazerox9@gmail.com>
2022-05-06 11:31:36 +0000

    Correction erreur ouss

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,7 +4,7 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		final int index;
-		for(int i = 0; i<10; i++) {
+		for(int i = 0; i<5; i++) {
 			System.out.println("Hello bugs !");
 		}
 		
